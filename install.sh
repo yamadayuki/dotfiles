0e65c477e4781bc358221caf9e3318164e7b7525
Yuki Yamada <yamadayuuki0818@gmail.com>
2016-11-20 09:00:30 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.vimrc.neobundle ~/.vimrc.neobundle
-ln -s ~/dotfiles/.vimrc.plug ~/.vimrc.plug
-ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+MY_PATH="`dirname \"$0\"`"
+DOTFILES_PATH="`( cd \"$MY_PATH\" && pwd )`"
+
+ln -s -i "$DOTFILES_PATH/.gitconfig" ~/.gitconfig
+ln -s -i "$DOTFILES_PATH/.vimrc" ~/.vimrc
+ln -s -i "$DOTFILES_PATH/.zshrc" ~/.zshrc
