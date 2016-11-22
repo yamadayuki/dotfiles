16b57fa7c2cd571f4f1a8836d81962a92dfb986e
Yuki Yamada <yamadayuuki0818@gmail.com>
2016-11-23 06:54:49 +0900

    Add .hyper.js to install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ MY_PATH="`dirname \"$0\"`"
 DOTFILES_PATH="`( cd \"$MY_PATH\" && pwd )`"
 
 ln -sf "$DOTFILES_PATH/.gitconfig" ~/.gitconfig
+ln -sf "$DOTFILES_PATH/.hyper.js" ~/.hyper.js
 ln -sf "$DOTFILES_PATH/.vimrc" ~/.vimrc
 ln -sf "$DOTFILES_PATH/.vimrc.plug" ~/.vimrc.plug
 ln -sf "$DOTFILES_PATH/.zshrc" ~/.zshrc
