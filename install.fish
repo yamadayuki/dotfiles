7a4a80ad7a7dd365db5dee99f6fbaf1f336bd8cf
Yuki Yamada <yamadayuuki0818@gmail.com>
2018-10-31 21:17:24 +0900

    Make more flexible installer

--- install.fish
+++ install.fish
@@ -16,27 +16,42 @@ function print_title
 end
 
 function link_files
-    echo "Link config files in dotfiles/config ."
+    echo "+ Link config files in dotfiles/config ."
 
-    set -l dotfiles_path "$PWD/config"
+    set -l dotfiles_path "$HOME/dotfiles/config"
     for file in (ls $dotfiles_path)
         ln -sf "$dotfiles_path/$file" "$HOME/.$file"
     end
 end
 
 function modify_defaults
-    echo "Modify macOS defaults."
+    echo "+ Modify macOS defaults."
 
     # Set a blazingly fast keyboard repeat rate.
     defaults write NSGlobalDomain KeyRepeat -int 1
     defaults write NSGlobalDomain InitialKeyRepeat -int 10
 end
 
+function install_via_homebrew
+    echo "+ Install modules via homebrew."
+
+    set -l commands bat fd flow fzf go hub imagemagick jq lua ocaml ocamlbuild opam creasty/tools/rid sqlite the_silver_searcher tree watchman yarn
+    brew install $commands
+end
+
+function install_via_fisher
+    echo "+ Install modules via fisher."
+
+    set -l packages jethrokuan/fzf rafaelrinaldi/pure yamadayuki/lure
+    fisher add $packages
+end
+
 function setup
+    echo "+ Setup environment variables."
+
     set -x GOPATH $HOME/dev
     set -x PATH $PATH $GOENV_ROOT/bin $GOPATH/bin
 end
-
 
 #  Endpoint
 #--------------------------------------
@@ -44,4 +59,6 @@ end
 print_title
 link_files
 modify_defaults
+install_via_homebrew
+install_via_fisher
 setup
