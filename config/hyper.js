9940e272536e63d2202815ee8658562aafa6555d
Yuki Yamada <yamadayuuki0818@gmail.com>
2019-03-11 23:14:33 +0900

    Update hyper.js config

--- config/hyper.js
+++ config/hyper.js
@@ -2,7 +2,6 @@ module.exports = {
   config: {
     // default font size in pixels for all tabs
     fontSize: 11,
-    // fontSize: 7,
 
     // font family with optional fallbacks
     // fontFamily: 'Ricty Diminished Discord',
@@ -10,18 +9,19 @@ module.exports = {
     uiFontFamily: "Operator Mono SSm",
 
     // terminal cursor background color (hex)
-    cursorColor: "white",
+    cursorColor: "#6C6F93",
+    cursorAccentColor: "#D5D8DA",
     cursorShape: "BLOCK",
     cursorBlink: true,
 
     // color of the text
-    foregroundColor: "#DBE6EC",
+    foregroundColor: "#D5D8DA",
 
     // terminal background color
-    backgroundColor: "#2C3643",
+    backgroundColor: "#1C1E26",
 
     // border color (window, tabs)
-    borderColor: "#3B444F",
+    borderColor: "#6BE4E6",
 
     // custom css to embed in the main window
     css: "",
@@ -34,24 +34,26 @@ module.exports = {
 
     // some color overrides. see http://bit.ly/29k1iU2 for
     // the full list
-    colors: [
-      "#000000",
-      "#5C6370",
-      "#E06C75",
-      "#E06C75",
-      "#98C379",
-      "#98C379",
-      "#D19A66",
-      "#D19A66",
-      "#61AFEF",
-      "#61AFEF",
-      "#C678DD",
-      "#C678DD",
-      "#56B6C2",
-      "#56B6C2",
-      "#ABB2BF",
-      "#FFFFFF",
-    ],
+    colors: {
+      // black: "",
+      red: "#E95678",
+      green: "#29D398",
+      yellow: "#FAB795",
+      blue: "#26BBD9",
+      magenta: "#EE64AC",
+      cyan: "#59E1E3",
+      white: "#D5D8DA",
+      // lightBlack: "",
+      lightRed: "#EC6A88",
+      lightGreen: "#3FDAA4",
+      lightYellow: "#FBC3A7",
+      lightBlue: "#3FC4DE",
+      lightMagenta: "#F075B5",
+      lightCyan: "#6BE4E6",
+      lightWhite: "#D5D8DA",
+      // colorCubes: "",
+      grayscale: "#6C6F93",
+    },
 
     shell: "/usr/local/bin/fish",
   },
@@ -62,13 +64,10 @@ module.exports = {
   //   `hypersolar`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [
-  ],
+  plugins: [],
 
   // in development, you can create a directory under
   // `~/.hyperterm_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
-  localPlugins: [
-    // 'tomorrow-night-hyper-dark'
-  ],
+  localPlugins: [],
 };
