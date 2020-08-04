a003f9954a442b90b13c22374edec0d97ea8069a
Yuki Yamada <yamadayuuki0818@gmail.com>
2020-08-04 09:37:33 +0900

    Workaround for the direnv load issue

--- config.fish
+++ config.fish
@@ -29,7 +29,24 @@ set -x EDITOR vim
 
 # direnv
 # eval (direnv hook fish)
-direnv hook fish | source
+function __direnv_export_eval --on-event fish_prompt
+    # Run on each prompt to update the state
+    "/usr/local/bin/direnv" export fish | source
+
+    # Handle cd history arrows between now and the next prompt
+    function __direnv_cd_hook --on-variable PWD
+        # ensure any output overwrites the prompt instead of going after it
+        echo -ne '\r'
+        # run the outer function to apply any changes
+        __direnv_export_eval
+    end
+
+    function __direnv_disable_cd --on-event fish_preexec
+        # Once we're running commands, stop monitoring cd changes
+        # until we get to the prompt again
+        functions --erase __direnv_cd_hook
+    end
+end
 
 # hub command config
 source (hub alias -s|psub)
