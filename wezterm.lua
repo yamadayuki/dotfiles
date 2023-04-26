de53ea9c521616c2516090457fb97dc0c9160b0a
Yamada Yuki <yamadayuuki0818@gmail.com>
2023-04-26 12:41:54 +0900

    Tweak

--- wezterm.lua
+++ wezterm.lua
@@ -4,51 +4,45 @@ return {
     font = wezterm.font({
         family = 'MonoLisa',
         weight = 'Medium',
-        harfbuzz_features = {'zero=1', 'liga=0', 'ss01=0', 'ss02=1', 'ss03=0'}
+        harfbuzz_features = { 'zero=1', 'liga=0', 'ss01=0', 'ss02=1', 'ss03=0' }
     }),
-
     default_cursor_style = 'SteadyBlock',
-
     colors = {
         foreground = '#b3b1ad',
         background = '#0d1117',
-
         cursor_fg = '#0d1117',
         cursor_bg = 'cyan',
         cursor_border = 'cyan',
-
         -- selection_fg = 'black',
         -- selection_bg = '#fffacd',
         -- scrollbar_thumb = '#222222',
 
         split = 'cyan',
-
-        ansi = {'484f58', -- black
-        'ff7b72', -- red
-        '3fb950', -- green
-        'd29922', -- yellow
-        '58a6ff', -- blue
-        'bc8cff', -- magenta
-        '39c5cf', -- cyan
-        'b1bac4' -- white
+        ansi = {
+            '484f58', -- black
+            'ff7b72', -- red
+            '3fb950', -- green
+            'd29922', -- yellow
+            '58a6ff', -- blue
+            'bc8cff', -- magenta
+            '39c5cf', -- cyan
+            'b1bac4'  -- white
         },
-
-        brights = {'6e7681', -- black
-        'ffa198', -- red
-        '56d364', -- green
-        'e3b341', -- yellow
-        '79c0ff', -- blue
-        'd2a8ff', -- magenta
-        '56d4dd', -- cyan
-        'f0f6fc' -- white
+        brights = {
+            '6e7681', -- black
+            'ffa198', -- red
+            '56d364', -- green
+            'e3b341', -- yellow
+            '79c0ff', -- blue
+            'd2a8ff', -- magenta
+            '56d4dd', -- cyan
+            'f0f6fc'  -- white
         },
-
         -- Arbitrary colors of the palette in the range from 16 to 255
         indexed = {
             [16] = '#d18616',
             [17] = '#ffa198'
         }
-
         -- Since: 20220319-142410-0fcdea07
         -- When the IME, a dead key or a leader key are being processed and are effectively
         -- holding input pending the result of input composition, change the cursor
@@ -74,26 +68,22 @@ return {
         -- quick_select_match_bg = { AnsiColor = 'Navy' },
         -- quick_select_match_fg = { Color = '#ffffff' },
     },
-
     window_frame = {
         font = wezterm.font({
             family = 'MonoLisa',
             weight = 'Medium',
-            harfbuzz_features = {'zero=1', 'liga=0', 'ss01=0', 'ss02=1', 'ss03=0'}
+            harfbuzz_features = { 'zero=1', 'liga=0', 'ss01=0', 'ss02=1', 'ss03=0' }
         }),
-
         font_size = 12.0
     },
-
     window_background_opacity = 1.0,
     hide_tab_bar_if_only_one_tab = true,
-
     leader = {
         key = 'k',
         mods = 'CMD',
         timeout_milliseconds = 1000
     },
-    keys = {{
+    keys = { {
         key = '\\',
         mods = 'CMD',
         action = wezterm.action.SplitHorizontal {
@@ -105,5 +95,5 @@ return {
         action = wezterm.action.SplitVertical {
             domain = 'CurrentPaneDomain'
         }
-    }}
+    } }
 }
